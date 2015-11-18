3e27fa8b88030e32aead5f7682a1df799d13d3b6
kinggggg <liweibooooo@126.com>
2015-11-18 22:50:45 +0800

    得到entityManagerFactory的第二种方式

--- src/test/java/com/zeek/jpa/CustomerTest.java
+++ src/test/java/com/zeek/jpa/CustomerTest.java
@@ -1,6 +1,8 @@
 package com.zeek.jpa;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -15,6 +17,10 @@ public class CustomerTest {
 	public void customerTest(){
 		String persistenceUnitName = "jpa-1" ;
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(persistenceUnitName);
+		//得到entityManagerFactory的第二种方式，通过这里设置的属性第一优先使用
+//		Map<String,String> map = new HashMap<String,String>();
+//		map.put("hibernate.show_sql", "false");
+//		entityManagerFactory = Persistence.createEntityManagerFactory(persistenceUnitName, map);
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		EntityTransaction transaction = entityManager.getTransaction();
 		transaction.begin();
